2326c1001197d688aa4659dcb20b766ccb196bd5
amaan562 <19B81A0562@cvr.ac.in>
2022-11-08 23:21:18 +0530

    prev bank details

--- src/components/BankDetails/BankDetails.js
+++ src/components/BankDetails/BankDetails.js
@@ -10,6 +10,10 @@ import Card from 'react-bootstrap/Card'
 import './BankDetails.css'
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+
+import {toast,ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function BankDetails() {
 
   useEffect(()=>{
@@ -28,42 +32,84 @@ function BankDetails() {
       console.log(url+" hehe here it is")
       Axios.get(url).then((data)=>{
         console.log(data)
+
         setPan(data.data.pan);
         setACNumber(data.data.accountNumber);
         setIfsc(data.data.ifsc);
         setFirstEmployment(data.data.firstEmployment);
-        if(pan!==null)  setNewUser(false);
-        console.log(newUser+" hehe first emp")
+        setPreviousCompany(data.data.previousCompany);
+        setPreviousRole(data.data.previousRole);
+        setPreviousSalary(data.data.previousSalary);
+
+        console.log("fetched: "+data);
+        // if(pan!==null)  setNewUser(false);
+        // console.log(newUser+" hehe first emp")
       })
   },[]) 
-  const [newUser,setNewUser] = useState(true);
+  // const [newUser,setNewUser] = useState(true);
+
   const [username,setUsername] = useState("");
   const [pan,setPan]=useState("")
   const [ACNumber,setACNumber]=useState("")
   const [ifsc, setIfsc] = useState("");
   const [firstEmployment, setFirstEmployment] = useState(false);
+
+  const [previousCompany, setPreviousCompany] = useState("");
+  const [previousSalary, setPreviousSalary] = useState();
+  const [previousRole, setPreviousRole] = useState("");
+
   const handleSubmit=async (e)=>{  
       e.preventDefault();
+
+      if(firstEmployment == true){
+          await setPreviousCompany(null);
+          await setPreviousRole(null);
+          await setPreviousSalary(null);
+      }
       var details = {
         username : username,
         pan : pan,
         ifsc : ifsc,
         accountNumber : ACNumber,
-        firstEmployment : firstEmployment
+        firstEmployment : firstEmployment,
+        previousCompany : previousCompany,
+        previousRole : previousRole,
+        previousSalary : previousSalary
       }
       var url = "http://localhost:8017/bankDetails";
-      if(!newUser){
+      // if(!newUser){
         url+="/"+username;
         var res = await Axios.put(url,details);
         console.log("put called");
-      }
-      else{
-        var res = await Axios.post(url,details);
-        console.log("post called");
-      }
+      // }
+      // else{
+      //   var res = await Axios.post(url,details);
+      //   console.log("post called");
+      // }
       console.log(res);
-      alert("Form submitted !!!");
+      toast.success("Form Submitted !!!")
   }
+
+  const prevEmployment = () => {
+      return(
+        <>
+            <br></br><br></br>
+            <Form.Group as={Row} className="mb-3" controlId="previousCompany" required>
+            <Col md={4}><Form.Label>Previous Company Name</Form.Label></Col>
+            <Col md={8}><Form.Control type="text" placeholder="Enter Company Name" onChange={e=>{setPreviousCompany(e.target.value)}}  value={previousCompany} /></Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="previousRole" required>
+            <Col md={4}><Form.Label>Previous Role</Form.Label></Col>
+            <Col md={8}><Form.Control type="text" placeholder="Enter Designation" onChange={e=>{setPreviousRole(e.target.value)}}  value={previousRole} /></Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="previousSalary" required>
+            <Col md={4}><Form.Label>Previous Salary</Form.Label></Col>
+            <Col md={8}><Form.Control type="number" placeholder="Enter Salary" onChange={e=>{setPreviousSalary(e.target.value)}}  value={previousSalary} /></Col>
+            </Form.Group>
+        </>
+      );
+  }
+
   return (
     <div className='bank'><Navbar/>
         <h1 style={{display: 'flex',  justifyContent:'center', margin: '50px 0' }}>PAN & Bank Details</h1>
@@ -100,6 +146,9 @@ function BankDetails() {
                       /> 
                       </Col>
                   </Form.Group>
+
+                  {!firstEmployment && prevEmployment()}
+
                   <Form.Group as={Row} className="mb-3">
                       <Button className="save"  type="submit" onClick={(e)=>handleSubmit(e)}>Submit</Button>
                   </Form.Group>
@@ -107,6 +156,7 @@ function BankDetails() {
           
             {/* </Card.Body> */}
         </div>
+        <ToastContainer />
     </div>
 
   )
